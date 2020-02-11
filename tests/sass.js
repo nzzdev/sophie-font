033ce922633712901fa48c42ddb8f537d195a177
Manuel Roth <manuelroth@hotmail.ch>
2020-02-11 16:54:19 +0100

    Release v1.3.2

--- tests/sass.js
+++ tests/sass.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
-const sass = require("node-sass");
+const sass = require("sass");
 const fs = require("fs");
 const jsonImporter = require("node-sass-json-importer");
 
 const files = fs.readdirSync("scss");
 files.forEach(function(fileName) {
   try {
-    sass.renderSync({
-      file: "scss/" + fileName,
-      includePaths: ["scss", "node_modules"],
-      importer: jsonImporter
-    });
+    console.log(
+      sass
+        .renderSync({
+          file: "scss/" + fileName,
+          includePaths: ["scss", "node_modules"],
+          importer: jsonImporter
+        })
+        .css.toString()
+    );
   } catch (err) {
     console.error(`failed compiling ${fileName}`);
     console.error(err);
