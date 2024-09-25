be54ce1111c235490bb779e1a84918890783b2d2
Kaspar Manz <kaspar.manz@nzz.ch>
2024-09-25 11:40:35 +0200

    chore: remove unneeded dependencies, upgrade sass version

--- tests/sass.js
+++ tests/sass.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 const sass = require("sass");
 const fs = require("fs");
-const jsonImporter = require("node-sass-json-importer");
 
 const files = fs.readdirSync("scss");
 files.forEach(function(fileName) {
   try {
     console.log(
       sass
-        .renderSync({
-          file: "scss/" + fileName,
-          includePaths: ["scss", "node_modules"],
-          importer: jsonImporter
-        })
+        .compile("scss/" + fileName)
         .css.toString()
     );
   } catch (err) {
